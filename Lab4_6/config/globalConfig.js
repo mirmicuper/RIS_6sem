621ae7c81c060bb7c572e44f098431d156da8639
NcUndstnd <Ignatcov169@gmail.com>
2024-05-17 02:50:03 +0300

    minor changes

--- Lab4_6/config/globalConfig.js
+++ Lab4_6/config/globalConfig.js
@@ -2,12 +2,12 @@ const globalConfig = {
     MAIN_SERVER: {
         PORT: 8000,
         STUDENT_NUMBER: 0,
-        ADDRESS: `http://localhost:8000`
+        ADDRESS: `http://192.168.43.141:8000`
     },
     LOCAL_SERVER_1: {
         PORT: 8001,
         STUDENT_NUMBER: 1,
-        ADDRESS: `http://localhost:8001`
+        ADDRESS: `http://192.168.43.159:8001`
     },
     LOCAL_SERVER_2: {
         PORT: 8002,
